9cf8a73ec47eac0ba4160fdce81981878637a1cf
Mike <tankf33der@disroot.org>
2020-05-30 18:47:10 +0300

    .

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ all: c a m u
 c:
 	rm -rf *.o *.0
 a:
-	clang -fsanitize=address main.c c25519.c f25519.c && ./a.out
+	clang -fsanitize=address *.c && ./a.out
 m:
-	clang -fsanitize=memory main.c c25519.c f25519.c && ./a.out
+	clang -fsanitize=memory *.c && ./a.out
 u:
-	clang -fsanitize=undefined main.c c25519.c f25519.c && ./a.out
+	clang -fsanitize=undefined *.c && ./a.out
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "c25519.h"
+#include "edsign.h"
 
 void x25519(void) {
 	uint8_t k[32] = {9};
@@ -11,8 +12,20 @@ void x25519(void) {
 	c25519_smult(r, k, u);
 }
 
+void ed25519(void) {
+    uint8_t k[32] = {123};
+    uint8_t p[32];
+    uint8_t m[32] = {123};
+    uint8_t s[64];
+
+    edsign_sec_to_pub(p, k);
+    edsign_sign  (s, p, k, m, 32);
+    edsign_verify(s, p,    m, 32);
+}
+
 int main() {
     x25519();
+    ed25519();
 	return 0;
 
 }
